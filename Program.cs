9f5fd47bc6a9438e0e334f3af9930550f1bc2090
potroshitel886 <154596022+potroshitel886@users.noreply.github.com>
2023-12-22 15:40:30 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество случайных фамилий: ");
+            if (int.TryParse(Console.ReadLine(), out int chislofamiliy))
+            {
+                NameGenerator nameGenerator = new NameGenerator();
+                for (int i = 0; i < chislofamiliy; i++)
+                {
+                    string name = nameGenerator.GenerateName();
+                    string surname = nameGenerator.GenerateSurname();
+                    string otchestvo = nameGenerator.GeneratePatronymic();
+                    Console.WriteLine($"{surname} {name} {otchestvo}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода числа.");
+            }
+            Console.ReadLine();
+        }
+     
+    }
+}
+        
+    
+
